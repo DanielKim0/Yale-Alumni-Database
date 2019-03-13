8c0a8bcc15d5d1751865e62fee1af5b0f04e0809
Daniel Kim <danielynskim@gmail.com>
2019-03-13 11:38:14 -0400

    Create update tests

--- spec/models/alumnus_spec.rb
+++ spec/models/alumnus_spec.rb
@@ -1,60 +1,111 @@
 require 'rails_helper'
 
 RSpec.describe Alumnus, type: :model do
-  describe 'basic validations' do
-    subject { build(:alumnus) }
+  context "when creating" do
+    describe 'basic validations' do
+      subject { build(:alumnus) }
 
-    it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
-    it { is_expected.to have_many(:attendances) }
+      it { is_expected.to validate_presence_of(:name) }
+      it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+      it { is_expected.to have_many(:attendances) }
+    end
+
+    describe 'other validations' do
+      it "has a valid factory" do
+        expect(FactoryBot.build(:alumnus).save).to be_truthy
+      end
+
+      it "is invalid without a name" do
+        expect(FactoryBot.build(:alumnus, name: nil).save).to be_falsey
+      end
+
+      it "is invalid with a whitespace name" do
+        expect(FactoryBot.build(:alumnus, name: "    ").save).to be_falsey
+      end
+
+      it "is valid even without a unique name" do
+        alumnus = FactoryBot.create(:alumnus)
+        expect(FactoryBot.build(:alumnus, email: "different@email.com").save).to be_truthy
+      end
+
+      it "is invalid without an email" do
+        expect(FactoryBot.build(:alumnus, email: nil).save).to be_falsey
+      end
+
+      it "is invalid without a unique email" do
+        alumnus = FactoryBot.create(:alumnus)
+        expect(FactoryBot.build(:alumnus, name: "Different Name").save).to be_falsey
+      end
+
+      it "should save email as lowercase" do
+        mixed_case_email = "TEST@test.TeSt"
+        alumnus = FactoryBot.create(:alumnus, email: mixed_case_email)
+        expect(alumnus.email).to eq(mixed_case_email.downcase)
+      end
+
+      it "is invalid without a unique lowercase email" do
+        mixed_case_email = "TEST@test.TeSt"
+        alumnus = FactoryBot.create(:alumnus)
+        expect(FactoryBot.build(:alumnus, email: mixed_case_email).save).to be_falsey
+      end
+
+      it "is invalid with a whitespace email" do
+        expect(FactoryBot.build(:alumnus, email: "    ").save).to be_falsey
+      end
+
+      it "is invalid without a correctly-formatted email" do
+        expect(FactoryBot.build(:alumnus, email: "abcd").save).to be_falsey
+      end
+    end
   end
 
-  describe 'other validations' do
+  context "when updating" do
+    describe "when valid" do
+      it "successfully updates name" do
+        alumnus = FactoryBot.create(:alumnus)
+        alumnus.update(name: "Other Name")
+        expect(alumnus.name).to eq("Other Name")
+      end
 
-    it "has a valid factory" do
-      expect(FactoryBot.build(:alumnus).save).to be_truthy
+      it "successfully updates email" do
+        alumnus = FactoryBot.create(:alumnus)
+        alumnus.update(email: "other@email.com")
+        expect(alumnus.email).to eq("other@email.com")
+      end
     end
 
-    it "is invalid without a name" do
-      expect(FactoryBot.build(:alumnus, name: nil).save).to be_falsey
-    end
+    describe "when not valid" do
+      let!(:alumnus) { FactoryBot.create(:alumnus) }
+      let!(:other) {FactoryBot.create(:alumnus, name: "Other Name", email: "other@email.com") }
 
-    it "is invalid with a whitespace name" do
-      expect(FactoryBot.build(:alumnus, name: "    ").save).to be_falsey
-    end
+      it "is invalid without a name" do
+        expect(alumnus.update(name: nil)).to be_falsey
+      end
 
-    it "is valid even without a unique name" do
-      alumnus = FactoryBot.create(:alumnus)
-      expect(FactoryBot.build(:alumnus, email: "different@email.com").save).to be_truthy
-    end
+      it "is invalid with a whitespace name" do
+        expect(alumnus.update(name: "    ")).to be_falsey
+      end
 
-    it "is invalid without an email" do
-      expect(FactoryBot.build(:alumnus, email: nil).save).to be_falsey
-    end
+      it "is invalid without an email" do
+        expect(alumnus.update(email: nil)).to be_falsey
+      end
 
-    it "is invalid without a unique email" do
-      alumnus = FactoryBot.create(:alumnus)
-      expect(FactoryBot.build(:alumnus, name: "Different Name").save).to be_falsey
-    end
+      it "is invalid without a unique email" do
+        expect(alumnus.update(email: "other@email.com")).to be_falsey
+      end
 
-    it "should save email as lowercase" do
-      mixed_case_email = "TEST@test.TeSt"
-      alumnus = FactoryBot.create(:alumnus, email: mixed_case_email)
-      expect(alumnus.email).to eq(mixed_case_email.downcase)
-    end
+      it "is invalid without a unique, case-insensitive email" do
+        mixed_case_email = "OTHER@email.CoM"
+        expect(alumnus.update(email: mixed_case_email)).to be_falsey
+      end
 
-    it "is invalid without a unique lowercase email" do
-      mixed_case_email = "TEST@test.TeSt"
-      alumnus = FactoryBot.create(:alumnus)
-      expect(FactoryBot.build(:alumnus, email: mixed_case_email).save).to be_falsey
-    end
+      it "is invalid with a whitespace email" do
+        expect(alumnus.update(email: "    ")).to be_falsey
+      end
 
-    it "is invalid with a whitespace email" do
-      expect(FactoryBot.build(:alumnus, email: "    ").save).to be_falsey
-    end
-
-    it "is invalid without a correctly-formatted email" do
-      expect(FactoryBot.build(:alumnus, email: "abcd").save).to be_falsey
+      it "is invalid without a correctly-formatted email" do
+        expect(alumnus.update(email: "abcd")).to be_falsey
+      end
     end
   end
 
