425d22f6cc2e326417b44d560a9449d6d6a9bb52
Daniel Kim <danielynskim@gmail.com>
2019-03-14 09:49:37 -0400

    Integrate theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,13 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require jquery_ujs
+//= require popper
+//= require material/bootstrap-material-design
+//= require material/perfect-scrollbar.jquery.min
+//= require material/chartist.min
+//= require material/arrive.min
+//= require material/bootstrap-notify
+//= require material/material-dashboard
+//= require material/demo
